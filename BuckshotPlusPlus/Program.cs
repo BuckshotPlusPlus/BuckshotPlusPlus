1a8b95fb7d36a4b3e74daccd552ae5841151d557
Vic92548 <victor.chanet@yahoo.com>
2020-11-18 21:33:36 +0100

    First commit

---
+++ BuckshotPlusPlus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuckshotPlusPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
