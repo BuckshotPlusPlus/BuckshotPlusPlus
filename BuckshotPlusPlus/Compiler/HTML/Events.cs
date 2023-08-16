b5d38cdb3532c0ff35153ada0e215f2c03525f57
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-08-16 18:56:52 +0200

    refactor: add missing events

--- BuckshotPlusPlus/Compiler/HTML/Events.cs
+++ BuckshotPlusPlus/Compiler/HTML/Events.cs
@@ -1,104 +1,129 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace BuckshotPlusPlus.Compiler.HTML
 {
     class Events
     {
-        // Form Events
-        public string onblur = "";
-        public string onchange = "";
-        public string oncontextmenu = "";
-        public string onfocus = "";
-        public string oninput = "";
-        public string oninvalid = "";
-        public string onreset = "";
-        public string onsearch = "";
-        public string onselect = "";
-        public string onsubmit = "";
+        static List<String> Props = new List<string>
+        {
+            // Window Events
+            "onafterprint",
+            "onbeforeprint",
+            "onbeforeunload",
+            "onerror",
+            "onhashchange",
+            "onload",
+            "onmessage",
+            "onoffline",
+            "ononline",
+            "onpagehide",
+            "onpageshow",
+            "onpopstate",
+            "onresize",
+            "onstorage",
+            "onunload",
 
-        // Keyboard Events
-        public string onkeydown = "";
-        public string onkeypress = "";
-        public string onkeyup = "";
+            // Form Events
+            "onblur",
+            "onchange",
+            "oncontextmenu",
+            "onfocus",
+            "oninput",
+            "oninvalid",
+            "onreset",
+            "onsearch",
+            "onselect",
+            "onsubmit",
 
-        // Mouse Events
-        public string onclick = "";
-        public string ondblclick = "";
-        public string onmousedown = "";
-        public string onmousemove = "";
-        public string onmouseout = "";
-        public string onmouseover = "";
-        public string onmouseup = "";
-        public string onmousewheel = "";
-        public string onwheel = "";
+            // Keyboard Events
+            "onkeydown",
+            "onkeypress",
+            "onkeyup",
 
-        // Drag Events
-        public string ondrag = "";
-        public string ondragend = "";
-        public string ondragenter = "";
-        public string ondragleave = "";
-        public string ondragover = "";
-        public string ondragstart = "";
-        public string ondrop = "";
-        public string onscroll = "";
+            // Mouse Events
+            "onclick",
+            "ondblclick",
+            "onmousedown",
+            "onmousemove",
+            "onmouseout",
+            "onmouseover",
+            "onmouseup",
+            "onmousewheel",
+            "onwheel",
 
-        // Clipboard Events
-        public string oncopy = "";
-        public string oncut = "";
-        public string onpaste = "";
+            // Drag Events
+            "ondrag",
+            "ondragend",
+            "ondragenter",
+            "ondragleave",
+            "ondragover",
+            "ondragstart",
+            "ondrop",
+            "onscroll",
 
-        // Media Events
-        public string onabort = "";
-        public string oncanplay = "";
-        public string oncanplaythrough = "";
-        public string oncuechange = "";
-        public string ondurationchange = "";
-        public string onemptied = "";
-        public string onended = "";
-        public string onerror = "";
-        public string onloadeddata = "";
-        public string onloadedmetadata = "";
-        public string onloadstart = "";
-        public string onpause = "";
-        public string onplay = "";
-        public string onplaying = "";
-        public string onprogress = "";
-        public string onratechange = "";
-        public string onseeked = "";
-        public string onseeking = "";
-        public string onstalled = "";
-        public string onsuspend = "";
-        public string ontimeupdate = "";
-        public string onvolumechange = "";
-        public string onwaiting = "";
+            // Clipboard Events
+            "oncopy",
+            "oncut",
+            "onpaste",
 
-        public static string GetHTMLEvents(List<Token> ServerSideTokens,Token MyToken)
+            // Media Events
+            "onabort",
+            "oncanplay",
+            "oncanplaythrough",
+            "oncuechange",
+            "ondurationchange",
+            "onemptied",
+            "onended",
+            "onerror",
+            "onloadeddata",
+            "onloadedmetadata",
+            "onloadstart",
+            "onpause",
+            "onplay",
+            "onplaying",
+            "onprogress",
+            "onratechange",
+            "onseeked",
+            "onseeking",
+            "onstalled",
+            "onsuspend",
+            "ontimeupdate",
+            "onvolumechange",
+            "onwaiting",
+
+            // Misc Events
+            "ontoggle"
+        };
+
+        public static string GetHTMLEvents(List<Token> ServerSideTokens, Token MyToken)
         {
             string CompiledEvents = " ";
-            //Formater.TokenCriticalError("LLLOOOOL", MyToken);
-            FieldInfo[] HTMLEvents = typeof(HTML.Events).GetFields();
+
             TokenDataContainer ViewContainer = (TokenDataContainer)MyToken.Data;
-            foreach (FieldInfo HTMLEvent in HTMLEvents)
+            foreach (String Name in Props)
             {
                 TokenDataVariable MyJSEventVar = TokenUtils.FindTokenDataVariableByName(
                     ViewContainer.ContainerData,
-                    HTMLEvent.Name
+                    Name
                 );
+
                 if (MyJSEventVar != null)
                 {
                     Token MyJSEvent = TokenUtils.FindTokenByName(
                         MyToken.MyTokenizer.FileTokens,
                         MyJSEventVar.GetCompiledVariableData(ServerSideTokens)
                     );
+
                     if (MyJSEvent != null)
                     {
                         CompiledEvents +=
-                            HTMLEvent.Name + "=\"" + JS.Event.GetEventString(ServerSideTokens,MyJSEvent) + "\" ";
-                        //Formater.TokenCriticalError("LLLOOOOL", MyJSEvent);
+                            Name + "=\"" + JS.Event.GetEventString(ServerSideTokens,MyJSEvent) + "\" ";
                     }
                 }
             }
+
             return CompiledEvents;
         }
     }
